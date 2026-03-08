47fd0da9662467754b4cbba6174727ebe36d905e
Jacob989801 <kuba.jelonkiewicz@gmail.com>
2026-03-08 18:20:59 +0100

    another small fixes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      
       '/contact': {
         target: 'https://aibridgebackend-production.up.railway.app',
         changeOrigin: true,
-        secure: true, 
+        secure: true,
       },
+      
       '/api': {
         target: 'https://aibridgebackend-production.up.railway.app',
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''), // usuwa /api z początku
       },
     },
   },
